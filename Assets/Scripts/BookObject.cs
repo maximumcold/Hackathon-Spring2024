a63c38ed84ae4d475d1546ed0f88c1afe83126b4
Maximum DeCorso <maxdecorso01@gmail.com>
2024-03-13 01:30:34 -0700

    Added Menu/Options to book object
    Added the ability to view a menu and make selections on the book object. Could be vastly improved by making a separate object class.

--- Assets/Scripts/BookObject.cs
+++ Assets/Scripts/BookObject.cs
@@ -29,56 +29,6 @@ public class BookObject : ObjectInteraction
         // Open menu of options
         Debug.Log("Interacting with book");
 
-        // Display options 
-        // 1. Read
-        // 2. Close
-        // 3. Take
-        // 4. Leave
-        // 5. Burn
-
-        // Create a canvas and panel for the popup
-        GameObject canvas = new GameObject("Canvas");
-        canvas.AddComponent<Canvas>();
-        canvas.AddComponent<CanvasScaler>();
-        canvas.AddComponent<GraphicRaycaster>();
-
-        GameObject panel = new GameObject("Panel");
-        panel.transform.SetParent(canvas.transform);
-        panel.AddComponent<Image>();
-
-        // Position the panel in the center of the screen
-        RectTransform panelRectTransform = panel.GetComponent<RectTransform>();
-        panelRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        panelRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        panelRectTransform.pivot = new Vector2(0.5f, 0.5f);
-        panelRectTransform.anchoredPosition = Vector2.zero;
-        panelRectTransform.sizeDelta = new Vector2(200f, 200f);
-
-        // Add buttons for each option
-        for (int i = 0; i < 5; i++)
-        {
-            GameObject button = new GameObject("Button" + (i + 1));
-            button.transform.SetParent(panel.transform);
-            button.AddComponent<Image>();
-            button.AddComponent<Button>();
-
-            // Position the button within the panel
-            RectTransform buttonRectTransform = button.GetComponent<RectTransform>();
-            buttonRectTransform.anchorMin = new Vector2(0.5f, 1f - (i + 1) * 0.2f);
-            buttonRectTransform.anchorMax = new Vector2(0.5f, 1f - i * 0.2f);
-            buttonRectTransform.pivot = new Vector2(0.5f, 0.5f);
-            buttonRectTransform.anchoredPosition = Vector2.zero;
-            buttonRectTransform.sizeDelta = new Vector2(180f, 30f);
-
-            // Add text to the button
-            GameObject buttonText = new GameObject("Text");
-            buttonText.transform.SetParent(button.transform);
-            buttonText.AddComponent<Text>();
-            Text textComponent = buttonText.GetComponent<Text>();
-            textComponent.text = "Option " + (i + 1);
-            textComponent.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-            textComponent.alignment = TextAnchor.MiddleCenter;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,35 +13,39 @@ public class Player : MonoBehaviour
     private float lastInteractionTime;
     Animator anim;
     
-    private bool is_Hidden = true;
+    private bool isHidden = true;
 
-    private bool is_Interacting = false;
+    private bool isInteracting = false;
+    private bool isShowingMenu = false;
+
+    public GameObject menuCanvas;
+    public TextMeshProUGUI menuText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Player is hidden: " + is_Hidden);
+        StartCoroutine(delay());
+        
+        Debug.Log("Player is hidden: " + isHidden);
         anim = GetComponent<Animator>();
+
+        menuCanvas = GameObject.Find("MenuCanvas");
+        menuCanvas.SetActive(false);
     }
 
     // Update is called once per frame
    private void Update() {
-        player_movement();
-
-
-    //     if (is_Interacting && Input.GetKeyDown(KeyCode.E)) {
-    //         Debug.Log("Interacting with object");
-    //         // Call the interaction logic  for the object
-    //         // Perform your interaction logic here
-    // }
+        playerMovement();
     }
 
-    void player_movement(){
+    void playerMovement(){
         
         // Switches between idle and walk animation
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
             GetComponent<Animator>().SetBool("isWalking", true);
+
             // Flip animtor based on direction
             if (Input.GetKey(KeyCode.A))
             {
@@ -57,7 +62,7 @@ public class Player : MonoBehaviour
         }
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+        // Move the player based on input
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
     }
@@ -79,33 +84,75 @@ public class Player : MonoBehaviour
                 {
                     interactableObject.Interact();
                     lastInteractionTime = Time.time; // Update the last interaction time
+                    menuCanvas.SetActive(true);
+                    
+                    menuText.verticalAlignment = VerticalAlignmentOptions.Top;
+                    menuText.text = "You have found a book! It probably contains some vital information about the case! Would be a shame is something were to happen to it...";
+                  
+                    menuText.text += "\nPress Space to continue";
+                    isShowingMenu = true;
                 }
             }
         }
-    }
-    private void OnCollisionExit2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Flashlight")) {
-            is_Hidden = true;
-            Debug.Log("Player is hidden: " + is_Hidden);
+        // Options for interacting with the book object (can be moved to a separate method if needed)
+        if (isShowingMenu && Input.GetButton("Continue"))
+        {
+            menuText.text = "1. Leave the book alone, it isn't worth tarnishing your reputation as a lizard lawyer.";
+            menuText.text += "\n2. Burn the book, let its forbidden knowledge be lost to time.";
+            menuText.text += "\n3. Tamper the evidence by writing someone else's name as the ownwer of the book.";
+        }
+
+        if (isShowingMenu && Input.GetButton("1"))
+        {
+            menuText.text = "You leave the book alone, it isn't worth tarnishing your reputation as a lizard lawyer.";
+            delay();
+        }
+        if (isShowingMenu && Input.GetButton("2"))
+        {
+            menuText.text = "You burn the book, its secrets are yours now.";
+            delay();
+            // Add code to destroy the book object that doesn't make it destroy immediately
+        }
+        if (isShowingMenu && Input.GetButton("3"))
+        {
+            menuText.text = "You write the name of your best friend into the book, Ezra Thomas Billings the Third.";
+            delay();
         }
     }
+
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Flashlight")) {
+            isHidden = true;
+            Debug.Log("Player is hidden: " + isHidden);
+        }
+    }
+
+
    private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Flashlight")) {
-            is_Hidden = false;
-            Debug.Log("Player is hidden: " + is_Hidden);
+            isHidden = false;
+            Debug.Log("Player is hidden: " + isHidden);
         }
    }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Flashlight")) {
-            is_Hidden = true;
-            Debug.Log("Player is hidden: " + is_Hidden);
+            isHidden = true;
+            Debug.Log("Player is hidden: " + isHidden);
         }
 
         if (other.CompareTag("Book")) {
-            is_Interacting = false;
+            isInteracting = false;
+            menuCanvas.SetActive(false);
         }
     }
 
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(10);
+
+        menuCanvas.SetActive(false);
+    }
 
 }
